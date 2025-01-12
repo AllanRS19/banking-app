177dce7b687b52ed292dbed96ceba74ee1cec0e6
Allan <allanrs19@gmail.com>
2025-01-12 13:20:59 -0400

    Implement Plaid Link and Dwolla

--- lib/utils.ts
+++ lib/utils.ts
@@ -204,7 +204,10 @@ export const authFormSchema = (type: string) => z.object({
   state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().refine((data) =>
+    (data.length === 9 || data.length === 4), {
+    message: 'SSN must contain 4 or 9 characters'
+  }),
   // both
   email: z.string().email(),
   password: z.string().min(8),
